e9f53f8cfb69e35430615e5b5f34ec8bd386af52
sveske_juice <66884216+Sveske-Juice@users.noreply.github.com>
2024-01-12 10:01:47 +0100

    Show ip in play

--- Assets/Scripts/BoatNetworkInput.cs
+++ Assets/Scripts/BoatNetworkInput.cs
@@ -1,18 +1,106 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using UnityEngine;
+
+[System.Serializable]
+public class InputUDPPacket
+{
+    char nw;
+    char ne;
+    char se;
+    char sw;
+
+    public override string ToString()
+    {
+        return $"NW: {nw}, NE: {ne}, SE: {se}, SW: {sw}";
+    }
+}
 
 public class BoatNetworkInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Thread receiveThread;
+
+    private UdpClient client;
+
+    [SerializeField]
+    private int port;
+
+    public string lastReceivedUDPPacket = "";
+    public string allReceivedUDPPackets = ""; // clean up this from time to time!
+
+    public void Start()
     {
-        
+        init();
     }
 
-    // Update is called once per frame
-    void Update()
+    // OnGUI
+    void OnGUI()
     {
-        
+        string ipv4 = NetworkManager.singleton.networkAddress;
+        Rect rectObj = new Rect(40, 10, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(
+            rectObj,
+            $"IP: {ipv4}",
+            style
+        );
+    }
+
+    private void init()
+    {
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+        while (true)
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                string text = Encoding.UTF8.GetString(data);
+
+
+                InputUDPPacket packet = ParseInput(text);
+                lastReceivedUDPPacket = packet.ToString();
+
+                allReceivedUDPPackets = allReceivedUDPPackets + packet.ToString();
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    public string getLatestUDPPacket()
+    {
+        allReceivedUDPPackets = "";
+        return lastReceivedUDPPacket;
+    }
+
+    private InputUDPPacket ParseInput(string inputmap)
+    {
+        try
+        {
+            InputUDPPacket packet = JsonUtility.FromJson<InputUDPPacket>(inputmap);
+            return packet;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Invalid input map packet from network controller: {inputmap}");
+            throw e;
+        }
+        return null;
     }
 }
