950d81abca80bfdeabfd6a8487db152d98becd64
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2024-02-18 00:44:08 +0100

    boat animation

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    public Animator animator;
+
     [SerializeField, Range(0f, 50f)]
     private float paddleForce = 15f;
 
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void Activate(Transform boatTransform, Rigidbody rb)
     {
+        animator.Play("Row");
         Vector3 force = (transform.parent.position - transform.position).normalized * paddleForce;
         force.y = 0f;
         //Vector2 force = boatTransform.forward * paddleForce;
